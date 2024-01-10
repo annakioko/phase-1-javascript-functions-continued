098676310206e106a0394b1e511523abfa6a8333
Anna Kioko <annakioko00@gmail.com>
2024-01-10 20:09:22 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    activity = 'roller-skate';
+  }
+  return 'This Saturday, I want to ' + activity + '!';
+}
+
+var mondayWork = function(activity) {
+  if (activity === undefined) {
+    activity = 'go to the office';
+  }
+  return 'This Monday, I will ' + activity + '.';
+};
+
+function wrapAdjective(flair) {
+  return function(adjective) {
+    return 'You are ' + flair + adjective + flair + '!';
+  };
+}
+
